045f4db8880cf848c4a5be2553f5ad8e20902daf
m081779 <marcoprincipio@hotmail.com>
2018-12-08 23:02:40 -0500

    cleaned up code, converted to es6 syntax, removed extraneous lines

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,9 @@
 const LocalStrategy    = require('passport-local').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
-const User       = require('../models/user');
+const User = require('../models/user');
 
 
-module.exports = function(passport) {
+module.exports = (passport) => {
 
   // =========================================================================
   // passport session setup ==================================================
@@ -12,16 +12,10 @@ module.exports = function(passport) {
   // passport needs ability to serialize and unserialize users out of session
 
   // used to serialize the user for the session
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
+  passport.serializeUser((user, done) => done(null, user.id));
 
   // used to deserialize the user
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
-  });
+  passport.deserializeUser((id, done) => User.findById(id, (err, user) => done(err, user)));
 
   // =========================================================================
   // LOCAL SIGNUP ============================================================
@@ -34,35 +28,30 @@ module.exports = function(passport) {
     passwordField     : 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) {
+  (req, email, password, done) => {
 
 
-      process.nextTick(function() {
+      process.nextTick(() => {
 
-          User.findOne({ 'local.email' :  email }, function(err, user) {
-              if (err)
-                  return done(err);
+          User.findOne({ 'local.email' :  email }, (err, user) => {
+              if (err) return done(err);
 
               if (user) {
                   return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
               } else {
 
-                  let newUser            = new User();
+                  const newUser            = new User();
                   newUser.local.email    = email;
                   newUser.local.password = newUser.generateHash(password);
 
                   // save the user
-                  newUser.save(function(err) {
-                      if (err)
-                          throw err;
+                  newUser.save((err) => {
+                      if (err) throw err;
                       return done(null, newUser);
                   });
               }
-
           });
-
       });
-
   }));
 
   // =========================================================================
@@ -76,18 +65,15 @@ module.exports = function(passport) {
     passwordField : 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) { // callback with email and password from our form
+  (req, email, password, done) => { // callback with email and password from our form
 
 
-      User.findOne({ 'local.email' :  email }, function(err, user) {
-          if (err)
-              return done(err);
+      User.findOne({ 'local.email' :  email }, (err, user) => {
+          if (err) return done(err);
 
-          if (!user)
-              return done(null, false, req.flash('loginMessage', 'No user found.'));
+          if (!user)  return done(null, false, req.flash('loginMessage', 'No user found.'));
 
-          if (!user.validPassword(password))
-              return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+          if (!user.validPassword(password)) return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
 
           return done(null, user);
       });
